142f8eeb297bf3894542a52cfa6186b5fc9df0bf
Kanzariya jayy <jaykanzariya153@gmail.com>
2026-08-20 11:53:52 +0530

    Time: 17 ms (80.83%), Space: 49.7 MB (35.96%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,35 +1,46 @@
-class Solution {
-    public int arrayPairSum(int[] nums) {
-        int sum=0;
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=0;j<nums.length-1-i;j++){
-                if(nums[j]>nums[j+1]){
-                int temp=nums[j];
-                nums[j]=nums[j+1];
-                nums[j+1]=temp;
-            }
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         int sum=0;
+//         for(int i=0;i<nums.length-1;i++){
+//             for(int j=0;j<nums.length-1-i;j++){
+//                 if(nums[j]>nums[j+1]){
+//                 int temp=nums[j];
+//                 nums[j]=nums[j+1];
+//                 nums[j+1]=temp;
+//             }
 
-            }
+//             }
             
-        }
+//         }
         
-        for(int i=0;i<nums.length-1;i++){
-                if(nums[i]<nums[i+1] ){
-                    sum=sum+nums[i];
-                }else if(nums[i+1]<nums[i]){
-                    sum=sum+nums[i+1];
-                }else{
-                    if(nums[i]==nums[i+1]){
-                        sum=sum+nums[i];
-                    }
-                }
-                i++;
-        }
-        return sum;
+//         for(int i=0;i<nums.length-1;i++){
+//                 if(nums[i]<nums[i+1] ){
+//                     sum=sum+nums[i];
+//                 }else if(nums[i+1]<nums[i]){
+//                     sum=sum+nums[i+1];
+//                 }else{
+//                     if(nums[i]==nums[i+1]){
+//                         sum=sum+nums[i];
+//                     }
+//                 }
+//                 i++;
+//         }
+//         return sum;
+//     }
+//     public static void main(String[] args) {
+//         Solution obj=new Solution();
+//          int nums[]={1,4,3,2};
+//         System.out.println(obj.arrayPairSum(nums)); 
+//     }
+// }
+
+class Solution {
+public int arrayPairSum(int[] nums) {
+    Arrays.sort(nums);
+    int result = 0;
+    for(int itr = 0; itr < nums.length; itr += 2){
+        result += nums[itr];
     }
-    public static void main(String[] args) {
-        Solution obj=new Solution();
-         int nums[]={1,4,3,2};
-        System.out.println(obj.arrayPairSum(nums)); 
-    }
+    return result;
+}
 }
