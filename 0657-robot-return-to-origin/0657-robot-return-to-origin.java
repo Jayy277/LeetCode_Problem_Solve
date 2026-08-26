5dc500c9604b5a11a413cbe74c599b44541337b7
Kanzariya jayy <jaykanzariya153@gmail.com>
2026-08-26 10:59:38 +0530

    Time: 6 ms (51.96%), Space: 45.6 MB (58.52%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,26 +1,46 @@
+// class Solution {
+//     public boolean judgeCircle(String moves) {
+//          int U=0;
+//         int D=0;
+//         int R=0;
+//         int L=0;
+//         HashSet <String> set =new HashSet<>();
+//         for(char ch:moves.toCharArray()){
+//             set.add(String.valueOf(ch));
+//             if(ch=='U'){
+//                 U++;
+//             }else if(ch=='D'){
+//                 D++;
+//             }else if(ch=='R'){
+//                 R++;
+//             }else if(ch=='L'){
+//                 L++;
+//             }
+//         }
+//         if(U==D & R==L){
+//             return true;
+//         }else{
+//             return false;
+//         }
+        
+//     }
+// }
+
+
+
 class Solution {
     public boolean judgeCircle(String moves) {
-         int U=0;
-        int D=0;
-        int R=0;
-        int L=0;
-        HashSet <String> set =new HashSet<>();
-        for(char ch:moves.toCharArray()){
-            set.add(String.valueOf(ch));
-            if(ch=='U'){
-                U++;
-            }else if(ch=='D'){
-                D++;
-            }else if(ch=='R'){
-                R++;
-            }else if(ch=='L'){
-                L++;
-            }
+        int x = 0, y = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+            char c = moves.charAt(i);
+
+            if (c == 'U') y++;
+            if (c == 'D') y--;
+            if (c == 'R') x++;
+            if (c == 'L') x--;
         }
-        if(U==D & R==L){
-            return true;
-        }else{
-            return false;
-        }
+
+        return x == 0 && y == 0;
     }
 }
