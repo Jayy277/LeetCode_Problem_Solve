cb049d25e4745e7319df06a2bf3fe9d7857d8e75
Kanzariya jayy <jaykanzariya153@gmail.com>
2026-08-27 15:27:56 +0530

    Time: 288 ms (67.56%), Space: 67.2 MB (30.44%) - LeetHub

---
+++ 2891-method-chaining/2891-method-chaining.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    df=animals.sort_values("weight",ascending=False)
+    
+    return df[df["weight"]>100][["name"]]
